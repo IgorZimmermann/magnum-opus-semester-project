f0d666a30059a4d6ea95b8205d03196a382b3cc1
Igor Zimmermann <igorzimmermann01@gmail.com>
2026-03-09 21:55:22 +0100

    create openapi compliant ping api endpoint

--- saga/src/index.ts
+++ saga/src/index.ts
@@ -1,10 +1,35 @@
 import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { createRoute, OpenAPIHono, z } from '@hono/zod-openapi'
 
-const app = new Hono()
+const app = new OpenAPIHono()
 
-app.get('/', (c) => {
-	return c.text('Hello Hono!')
+app.openapi(
+	createRoute({
+		method: 'get',
+		path: '/ping',
+		responses: {
+			200: {
+				description: 'Sends back pong to check connection',
+				content: {
+					'application/text': {
+						schema: z.string(),
+					},
+				},
+			},
+		},
+	}),
+	(c) => {
+		return c.text('pong')
+	},
+)
+
+app.doc('/doc', {
+	openapi: '3.0.0',
+	info: {
+		version: '1.0.0',
+		title: 'Saga API',
+		description: 'PDF Generation API',
+	},
 })
 
 serve(
