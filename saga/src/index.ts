458e71bf6d476018c11c4a2fda4e006780e5d3f0
Igor Zimmermann <igorzimmermann01@gmail.com>
2026-03-14 13:04:40 +0100

    add comments to index.ts

--- saga/src/index.ts
+++ saga/src/index.ts
@@ -4,8 +4,10 @@ import { execFileSync } from 'child_process'
 import { randomUUID } from 'crypto'
 import { readFileSync, rmSync } from 'fs'
 
+// Creating OpenAPI Hono application instance
 const app = new OpenAPIHono()
 
+// Creating documentation route to serve OpenAPI specification
 app.doc('/doc', {
 	openapi: '3.0.0',
 	info: {
@@ -15,7 +17,9 @@ app.doc('/doc', {
 	},
 })
 
+// Creating a ping route to check connection to API
 app.openapi(
+	// Describing route and reponses for OpenAPI documentation
 	createRoute({
 		method: 'get',
 		path: '/ping',
@@ -31,11 +35,14 @@ app.openapi(
 		},
 	}),
 	(c) => {
+		// Returning pong
 		return c.text('pong')
 	},
 )
 
+// Creating a generate route that generates the doctor's note
 app.openapi(
+	// Describing route, request and responses for OpenAPI documentation
 	createRoute({
 		method: 'post',
 		path: '/generate',
@@ -43,6 +50,7 @@ app.openapi(
 			body: {
 				content: {
 					'application/json': {
+						// Creating a zod schema for request body validation
 						schema: z.object({
 							doctor: z.object({
 								name: z.string(),
@@ -65,6 +73,7 @@ app.openapi(
 				description: 'Generates PDF and returns the file buffer',
 				content: {
 					'application/pdf': {
+						// Creating zod schema for 200 response and describing it for OpenAPI
 						schema: z.instanceof(Buffer).openapi({
 							type: 'string',
 							format: 'binary',
@@ -76,6 +85,7 @@ app.openapi(
 				description: 'PDF Generation process fails',
 				content: {
 					'application/json': {
+						// Creating zod schema for 500 response
 						schema: z.object({
 							message: z.string(),
 						}),
@@ -85,10 +95,15 @@ app.openapi(
 		},
 	}),
 	(c) => {
+		// Getting validated data from request body
 		const data = c.req.valid('json')
+		
+		// Generating random UUID for filename
 		const id = randomUUID()
 
+		// Generating PDF using typst cli
 		try {
+			// Executing typst command, outputting to generated id
 			execFileSync('typst', [
 				'compile',
 				'--input',
@@ -97,24 +112,31 @@ app.openapi(
 				`temp/${id}.pdf`,
 			])
 		} catch {
+			// Returning 500 error and message if process fails
 			return c.json({ message: 'error creating pdf' }, 500)
 		}
 
+		// Reading PDF file and returning 200 success and file buffer
 		try {
+			// Reading file using the generated id
 			const buffer = readFileSync(`temp/${id}.pdf`)
 
+			// Return 200 success and buffer, along with headers about the content
 			return c.body(buffer, 200, {
 				'Content-Type': 'application/pdf',
 				'Content-Disposition': 'attachment; filename="output.pdf"',
 			})
 		} catch {
+			// Returning 500 error and message if process fails
 			return c.json({ message: 'error reading file' }, 500)
 		} finally {
+			// Removing temporary PDF file based on generated id
 			rmSync(`temp/${id}.pdf`, { force: true })
 		}
 	},
 )
 
+// Serving application on port 3000
 serve(
 	{
 		fetch: app.fetch,
