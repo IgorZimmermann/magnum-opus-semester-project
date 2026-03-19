8d7153a0832fa06e59e6b13334411131aaa08a54
seala2005 <sean.larsen0408@gmail.com>
2026-03-19 10:45:07 +0100

    Added services & interface to program.cs

--- heimdall/ConsultationBackend/Program.cs
+++ heimdall/ConsultationBackend/Program.cs
@@ -1,4 +1,13 @@
+using ConsultationBackend.Interfaces.Services;
+using ConsultationBackend.Services;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IPrescriptionService, PrescriptionService>();
+builder.Services.AddScoped<ISummaryService, SummaryService>();
+builder.Services.AddScoped<ITranscriptService, TranscriptService>();
+builder.Services.AddScoped<IConsultationService, ConsultationService>();
 
 // Add services to the container.
 
