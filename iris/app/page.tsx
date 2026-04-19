d2f8077dd7fe866dd1bb1419f47abe01cc6e83b2
akos-koltai <koltaikristofakos@gmail.com>
2026-04-19 04:41:40 +0200

    add login button ui

--- iris/app/page.tsx
+++ iris/app/page.tsx
@@ -1,19 +1,23 @@
 import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 export default function Page() {
-  return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
-      </div>
-    </div>
-  )
+	const isLoggedIn = true
+
+	return (
+		<div>
+			{isLoggedIn ? (
+				<div className="w-dvw h-dvh flex items-center justify-center">
+					<div className="flex flex-col items-center gap-5">
+						<p>Welcome! Please authenticate!</p>
+						<Link href="/booking">
+							<Button>Authenticate</Button>
+						</Link>
+					</div>
+				</div>
+			) : (
+				<span>logged in</span>
+			)}
+		</div>
+	)
 }
